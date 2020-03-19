9ea4855dddb3f0142eff08c728bd46614fd3759f
Elismar13 <elismarsilva.5@outlook.com>
2020-03-19 14:36:11 -0300

    :pencil: Cominucacao com a API

--- src/api/games.js
+++ src/api/games.js
@@ -1,6 +1,16 @@
 const games = {
-    MetalGear: {
-        nome:"Metal Gear Solid 3 : Snake Eater",
-        
+    phantasystar: {
+        nome:"Phantasy Star",
+        descricao:"Um RPG de 1987 que marcou minha infância. Um dos primeiros jogos que joguei."
+    },
+    metalgear: {
+        nome:"Phantasy Star",
+        descricao:"Minha saga favorita no mundo dos games. Não tenho palavras para descrever minha experiência com a franquia Metal Gear."
+    },
+    lis:{
+        nome:"Life is Strange",
+        descricao: "O jogo que marcou minha vida. Me ensinou a dar valor a coisas pequenas e aproveitar cada momento como se fosse o último. "
     }
 }
+
+export default games;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Image} from 'react';
 import './styles.css';
 
 import api from '../../api/githubProfile';
@@ -8,31 +8,39 @@ const Home = () => {
     const [repositorios, setRepo] = useState({});
 
     useEffect( () => {
-        async function Solicitacao( url ) {
-            const response = await api.get( url );
+        async function getUser() {
+            const UserRequest = await api.get("/Elismar13");
+            setUser(UserRequest.data); //login, html_url, name, bio, avatar_url
         }
 
-        const user = Solicitacao("/Elismar13");
-        const repositorios = Solicitacao("/Elismar13/repos?&sort=update");
+        async function getRepositorios() {
+            const RepoRequest = await api.get("/Elismar13/repos?&sort=update");
+            setRepo(RepoRequest.data);
+        }
+        
+        getUser();
+        getRepositorios();
 
-        //setUser(user);
-        //setRepo(repositorios);
 
-        console.log(user, repositorios)
-     } )
+        console.log(user)
+     }, [] )
     return (
             <div className="Container">
-                <h1 className="MeuNome">Elismar13</h1>
-                    <section className="Descricao">
-                        <p>Olá! Me chamo Elismar Silva Pereira, tenho 18 anos e sou de uma pequena cidade 
-                            localizada no agreste paraibano.</p>
-                        <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
-                            foi daí que surgiu minha paixão por estas magníficas áreas.</p>
-                        <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
-                            Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
-                            no curso superior de Engenharia de Computação (2019.1).</p>
-                    </section>
+                <img src={user.avatar_url} className="MinhaImagem" />
+                <h1 className="MeuNome">{user.login}</h1>
 
+                    <div className="Informacoes">
+                        <section className="Descricao">
+                            <p>Olá! Me chamo Elismar Silva Pereira, tenho 18 anos e sou de uma pequena cidade 
+                                localizada no agreste paraibano.</p>
+                            <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
+                                foi daí que surgiu minha paixão por estas magníficas áreas.</p>
+                            <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
+                                Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
+                                no curso superior de Engenharia de Computação (2019.1).</p>
+                        </section>
+                    </div>
+                    
             </div>
     )
 };
