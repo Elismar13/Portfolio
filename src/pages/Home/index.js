bbb4de91b5e3b42ab326d58021460022d0ca4a3d
Elismar13 <elismarsilva.5@outlook.com>
2020-03-19 10:38:52 -0300

    :bump: minha descricao

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,9 +3,22 @@ import './styles.css';
 
 
 const Home = () => (
-    <div>
-        <h1>Hello, World!</h1>
-    </div>
+    <body>
+        <div className="Container">
+            <h1 className="MeuNome">Elismar13</h1>
+                <section className="Descricao">
+                    <p>Olá! Me chamo Elismar Silva Pereira, tenho 18 anos e sou de uma pequena cidade 
+                        localizada no agreste paraibano.</p>
+                    <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
+                        foi daí que surgiu minha paixão por estas magníficas áreas.</p>
+                    <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
+                        Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
+                        no curso superior de Engenharia de Computação (2019.1).</p>
+                </section>
+
+        </div>
+    </body>
+
 );
 
 export default Home;
