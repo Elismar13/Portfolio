9ad29c92cd06955240962020f236c504a58f51ea
Elismar13 <elismarsilva.5@outlook.com>
2020-03-19 11:59:31 -0300

    Alguns games

---
+++ src/api/games.js
@@ -0,0 +1,6 @@
+const games = {
+    MetalGear: {
+        nome:"Metal Gear Solid 3 : Snake Eater",
+        
+    }
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles.css';
 
+import api from '../../api/githubProfile';
 
-const Home = () => (
-    <body>
-        <div className="Container">
-            <h1 className="MeuNome">Elismar13</h1>
-                <section className="Descricao">
-                    <p>Olá! Me chamo Elismar Silva Pereira, tenho 18 anos e sou de uma pequena cidade 
-                        localizada no agreste paraibano.</p>
-                    <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
-                        foi daí que surgiu minha paixão por estas magníficas áreas.</p>
-                    <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
-                        Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
-                        no curso superior de Engenharia de Computação (2019.1).</p>
-                </section>
+const Home = () => {
+    const [user, setUser] = useState({});
+    const [repositorios, setRepo] = useState({});
 
-        </div>
-    </body>
+    useEffect( () => {
+        async function Solicitacao( url ) {
+            const response = await api.get( url );
+        }
 
-);
+        const user = Solicitacao("/Elismar13");
+        const repositorios = Solicitacao("/Elismar13/repos?&sort=update");
+
+        //setUser(user);
+        //setRepo(repositorios);
+
+        console.log(user, repositorios)
+     } )
+    return (
+            <div className="Container">
+                <h1 className="MeuNome">Elismar13</h1>
+                    <section className="Descricao">
+                        <p>Olá! Me chamo Elismar Silva Pereira, tenho 18 anos e sou de uma pequena cidade 
+                            localizada no agreste paraibano.</p>
+                        <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
+                            foi daí que surgiu minha paixão por estas magníficas áreas.</p>
+                        <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
+                            Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
+                            no curso superior de Engenharia de Computação (2019.1).</p>
+                    </section>
+
+            </div>
+    )
+};
 
 export default Home;
