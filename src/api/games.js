d8fbb717a41c628d567095e5c83a453f95a71804
Elismar13 <elismarsilva.5@outlook.com>
2020-03-19 20:01:44 -0300

    :pencil: pequena alteracao nos jogos

--- src/api/games.js
+++ src/api/games.js
@@ -1,6 +1,8 @@
 import Phantasy from '../assets/phantasy.png';
 import MGS from '../assets/mgsv.jpg';
 import LiS from '../assets/LiS.jpg'
+import Mario from '../assets/mario.jpg';
+import RE from '../assets/residentevil3.jpg'
 
 
 import React from 'react';
@@ -12,6 +14,16 @@ const games = [
         foto:Phantasy
     },
     {
+        nome:"Super Mario World",
+        descricao:"Meu primeiro jogo de plataforma. Foi uma experiência muito divertida, fechei este jogo várias vezes e nunca enjoei.",
+        foto:Mario
+    },
+    {
+        nome:"Resident Evil",
+        descricao:"Primeiro jogo de terror que joguei. Levei muitos sustos quando pequeno, mas no fim virei fã da franquia.",
+        foto:RE
+    },
+    {
         nome:"Metal Gear Solid",
         descricao:"Minha saga favorita no mundo dos games. Não tenho palavras para descrever minha experiência com a franquia Metal Gear.",
         foto:MGS
@@ -20,7 +32,8 @@ const games = [
         nome:"Life is Strange",
         descricao: "O jogo que marcou minha vida. Me ensinou a dar valor a coisas pequenas e aproveitar cada momento como se fosse o último. ",
         foto:LiS
-    }
+    },
+
 ];
 
 export default games;
