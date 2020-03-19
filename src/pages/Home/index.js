edd474fb03a75eea5d612fba71a8f1c3ffa881e7
Elismar13 <elismarsilva.5@outlook.com>
2020-03-19 15:28:39 -0300

    :pencil: lista de reposistorios

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,7 +5,7 @@ import api from '../../api/githubProfile';
 
 const Home = () => {
     const [user, setUser] = useState({});
-    const [repositorios, setRepo] = useState({});
+    const [repositorios, setRepo] = useState([]);
 
     useEffect( () => {
         async function getUser() {
@@ -29,18 +29,28 @@ const Home = () => {
                 <img src={user.avatar_url} className="MinhaImagem" />
                 <h1 className="MeuNome">{user.login}</h1>
 
-                    <div className="Informacoes">
-                        <section className="Descricao">
-                            <p>Olá! Me chamo Elismar Silva Pereira, tenho 18 anos e sou de uma pequena cidade 
-                                localizada no agreste paraibano.</p>
-                            <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
-                                foi daí que surgiu minha paixão por estas magníficas áreas.</p>
-                            <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
-                                Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
-                                no curso superior de Engenharia de Computação (2019.1).</p>
-                        </section>
-                    </div>
-                    
+                    <section className="Descricao">
+                        <p>Olá! Me chamo <strong>Elismar Silva Pereira</strong>, tenho 18 anos e sou de uma pequena cidade 
+                            localizada no agreste paraibano.</p>
+                        <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
+                            foi daí que surgiu minha paixão por estas magníficas áreas.</p>
+                        <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
+                            Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
+                            no curso superior de Engenharia de Computação (2019.1).</p>
+                    </section>
+
+                <h3>Minhas contribuições:</h3>
+                    <section className="Repositorios">
+                        { repositorios.map((value, id) => {
+                            return(
+                                <div className="ItemRepositorio" key={id}>
+                                    <p className="NomeRepositorio">{value.name}</p>
+                                    <p className="DescricaoRepositorio">{value.description === null ? "Sem descricão" : value.description}</p>
+                                    <p className="TecnologiasRepositorios">{value.language === null ? "Multiplas" : value.language}</p>
+                                </div>
+                            )
+                        })}
+                    </section>
             </div>
     )
 };
