2792235e12070c8f6b2025e2424b600d345c7225
Elismar Sivla <elismarsilva.5@outlook.com>
2020-04-05 10:45:18 -0300

    :pencil: Campo de descricao

---
+++ src/components/Description/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './styles.css';
+
+// eslint-disable-next-line react/prop-types
+export default function Description({ user = {} }) {
+    return (
+        <div className="description">
+            <div className="description-image">
+                <img src={user.avatar_url} className="MinhaImagem" />
+                <p> {user.login} </p>
+            </div>
+            <div className="description-text">
+                <p>Olá! Me chamo <strong>Elismar Silva Pereira</strong>, tenho 18 anos e sou de uma pequena cidade 
+                    localizada no agreste paraibano.</p>
+                <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
+                    foi daí que surgiu minha paixão por estas magníficas áreas.</p>
+                <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
+                    Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
+                    no curso superior de Engenharia de Computação (2019.1).</p>
+            </div>
+        </div>
+    )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,6 +6,7 @@ import './styles.css';
 import api from '../../api/githubProfile';
 
 import Hello from '../../components/Hello';
+import Description from '../../components/Description';
 import Techs from '../../components/Techs';
 import Repositories from '../../components/Repositories';
 import Games from '../../components/Games';
@@ -38,29 +39,13 @@ const Home = () => {
             <div className="container">
                 <Hello />
                 
-                <div className="description">
-                    <div className="description-image">
-                        <img src={user.avatar_url} className="MinhaImagem" />
-                        <p> {user.login} </p>
-                    </div>
-                    <div className="description-text">
-                        <p>Olá! Me chamo <strong>Elismar Silva Pereira</strong>, tenho 18 anos e sou de uma pequena cidade 
-                            localizada no agreste paraibano.</p>
-                        <p>Desde pequeno sempre gostei de assuntos que envolviam tecnologia, mais especificadamente programação e eletrônica, 
-                            foi daí que surgiu minha paixão por estas magníficas áreas.</p>
-                        <p>Posteriormente conclui um curso técnico na área de Telecomunicações, onde tive a oportunidade de entrar de vez neste mundo da tecnologia. 
-                            Atualmente sigo minha longa jornada no Instituto Federal da Paraíba - Campus Campina Grande, 
-                            no curso superior de Engenharia de Computação (2019.1).</p>
-                    </div>
-                </div>
+                <Description user={user} />
 
                 <Techs />
 
                 <Repositories repositorios={repositorios} />
 
                 <Games />
-
-
             </div>
     )
 };
