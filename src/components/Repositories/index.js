0be6e6787942d960625e7569dfd373ab92ef7cf0
Elismar Sivla <elismarsilva.5@outlook.com>
2020-04-12 12:24:06 -0300

    :pencil: carregar mais repositórios

--- src/components/Repositories/index.js
+++ src/components/Repositories/index.js
@@ -6,37 +6,29 @@ import './styles.css';
 export default function Repositories( {repositorios} ) {
     const Redireciona = (url) => window.open(url);
 
-    const [repos, setRepo] = useState([]);
-    const [reponumber, setNumber] = useState(0);
-
-    useEffect(() => {
-        function setRepoData() {
-            setRepo(repositorios.slice(0, 5*reponumber));
-            console.log(reponumber)
-        }
-
-        setRepoData();
-    }, [reponumber]);
-
+    const [reponumber, setNumber] = useState(1);
 
     return (
         <div className="repositorios">
             <h3>Minhas contribuições</h3>
                 <section>
-                    { repos.map((value, id) => {
-                        return(
-                            <div className="repositorios-item" 
-                                 key={id}  
-                                 onClick={() => {Redireciona(value.html_url)}}
-                                >
-                                <p className="repositorios-name">{value.name}</p>
-                                <p className="repositorios-description">
-                                    {value.description === null ? "Estou sem descição :(" : value.description}
-                                </p>
-                                <p className="repositorios-techs">{value.language === null ? "Multiplas" : value.language}</p>
-                            </div>
-                        )
-                    })}
+                    { repositorios
+                        .slice(0, 5*reponumber)
+                        .map((value, id) => {
+                            return(
+                                <div className="repositorios-item" 
+                                    key={id}  
+                                    onClick={() => Redireciona(value.html_url)}
+                                    >
+                                    <p className="repositorios-name">{value.name}</p>
+                                    <p className="repositorios-description">
+                                        {value.description === null ? "Estou sem descição :(" : value.description}
+                                    </p>
+                                    <p className="repositorios-techs">{value.language === null ? "Multiplas" : value.language}</p>
+                                </div>
+                            )
+                        })
+                    }
                 </section>
                 <button 
                     className="mostrarmais"
