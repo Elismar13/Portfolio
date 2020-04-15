9ee05602c1f8f14ce5abaae09ecb20e3d0c5c5e5
Elismar Sivla <elismarsilva.5@outlook.com>
2020-04-14 21:07:35 -0300

    :pencil: algumas mudanças na responsividade

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -15,11 +15,11 @@ export default function Hello() {
             <p>Estudante de engenharia de computação e fascinado por projetos open-source.</p>
             <div className="links">
                 <a href="https://github.com/Elismar13"> 
-                    <FaGithub size={60} title="GitHub"/> </a>
-                <a> <FaLinkedin size={60} title="Linkedin"/> </a>
-                <a> <FaTwitter size={60} title="Twitter"/> </a>
-                <a> <FaInstagram size={60} title="Instagram"/> </a>
-                <a> <FaFacebook size={60}  title="Facebook" />  </a> 
+                    <FaGithub    size={60}   title="GitHub"/> </a>
+                <a> <FaLinkedin  size={60}   title="Linkedin"/> </a>
+                <a> <FaTwitter   size={60}   title="Twitter"/> </a>
+                <a> <FaInstagram size={60}   title="Instagram"/> </a>
+                <a> <FaFacebook  size={60}   title="Facebook" />  </a> 
             </div>
         </div>
     );
