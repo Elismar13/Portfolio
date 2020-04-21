e4dc10e4eab6120cd4e0dae4da816e2af8c518ae
Elismar Sivla <elismarsilva.5@outlook.com>
2020-04-21 13:16:44 -0300

    :pencil: (fear): animations

--- src/components/Hello/index.js
+++ src/components/Hello/index.js
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaInstagram, 
          FaLinkedin, 
          FaFacebook, 
          FaTwitter, 
-         FaGithub } from 'react-icons/fa';
+         FaGithub,
+         FaAngleDoubleDown } from 'react-icons/fa';
 
 import './styles.css';
 
 
 export default function Hello() {
+    const [size, setSize] = useState(60);
+
+    function scroll(taxe) {
+        const heigth = window.screen.height;
+        window.scrollTo(0, heigth);
+    }
+
+    useEffect(() => {
+        if(window.screen.width < 700) {
+            setSize(40);
+        }
+    });
+
     return (
         <div className="header">
                 <h1>Olá, Me chamo <strong>Elismar</strong>!</h1>
             <p>Estudante de engenharia de computação e fascinado por projetos open-source.</p>
             <div className="links">
                 <a href="https://github.com/Elismar13"> 
-                    <FaGithub    size={60}   title="GitHub"/> </a>
-                <a> <FaLinkedin  size={60}   title="Linkedin"/> </a>
-                <a> <FaTwitter   size={60}   title="Twitter"/> </a>
-                <a> <FaInstagram size={60}   title="Instagram"/> </a>
-                <a> <FaFacebook  size={60}   title="Facebook" />  </a> 
+                    <FaGithub    size={size}   title="GitHub"/> </a>
+                <a> <FaLinkedin  size={size}   title="Linkedin"/> </a>
+                <a> <FaTwitter   size={size}   title="Twitter"/> </a>
+                <a> <FaInstagram size={size}   title="Instagram"/> </a>
+                <a> <FaFacebook  size={size}   title="Facebook" />  </a> 
+            </div>
+            <div className="scroll-button">
+                <button onClick={() => scroll(5)}>
+                    <FaAngleDoubleDown size={20} color="#ffffff" />
+                </button>
             </div>
         </div>
     );
