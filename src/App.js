a5b3751c296c18ee3c198f65797e541a79e5ebe8
Elismar13 <elismarsilva.5@outlook.com>
2020-07-20 23:01:18 -0300

    refactor: 💡 Code refactor

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
-
 import Routes from './routes';
-
 import './global.css';
 
 const App = () => (
-    <>
-        <Routes />
-    </>
+    <Routes />
 );
 
 export default App;
